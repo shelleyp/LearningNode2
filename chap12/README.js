bd381ce7bfc702dc83cfc2d8fa33d920ebe1eac4
Shelley Powers <shelley.just@gmail.com>
2018-03-20 14:44:29 -0500

    Modified README

---
+++ chap12/README.js
@@ -0,0 +1,10 @@
+# Learning Node 2nd edition
+
+## Updates to examples for upcoming Node.js 10 release
+
+### Chapter 12
+
+Support for Node on smart devices continues. Node running on ChakraCore is still a work in progress, but is actively supported. So is Samsung''s IoT.js work. 
+
+Support for Node on Arduinos and Raspberry Pi has changed little other than increased support. 
+
